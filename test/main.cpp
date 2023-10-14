4040a02d629f7de90ff10971587ae64af81fdc8e
Diogenes Cristaldo <diogenescristaldo@gmail.com>
2023-10-13 22:01:59 -0300

    Adding case test for graph

---
+++ test/main.cpp
@@ -0,0 +1,22 @@
+#include "graph.hpp"
+
+int main() {
+    
+    std::string as = "Grafoa";
+    toha::graph<int> g(25, "Grafo-a", DIRECTED_GRAPH);
+    g.add(1);
+    g.add(2);
+    g.add(3);
+    g.add(4);
+    g.add(26);
+    g.add(27);
+
+    g.connect(1,2);
+    g.connect(1,4);
+    g.connect(1,25);
+    g.connect(1,26);
+    g.connect(1,27);
+
+    g.showConnections();
+    return 0;
+}
